abff4056905d4f7497102d36ebd3973c3b367332
naila-ady <164941279+naila-ady@users.noreply.github.com>
2025-05-28 19:21:55 +0500

    created first simple agent

---
+++ main.py
@@ -0,0 +1,36 @@
+import asyncio
+from openai import AsyncOpenAI
+from agents import Agent, OpenAIChatCompletionsModel, Runner, set_tracing_disabled
+import os
+
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+BASE_URL = os.getenv("BASE_URL")
+MODEL = os.getenv("MODEL")
+
+
+
+client = AsyncOpenAI(
+    api_key=OPENROUTER_API_KEY,
+    base_url=BASE_URL
+)
+
+set_tracing_disabled(disabled=True)
+
+async def main():
+    # This agent will use the custom LLM provider
+    agent = Agent(
+        name="Assistant",
+        instructions="You only respond in haikus.",
+        model=OpenAIChatCompletionsModel(model=MODEL, openai_client=client),
+    )
+
+    result = await Runner.run(
+        agent,
+        "what is the capital of Pakistan?",
+    )
+    print(result.final_output)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+     
